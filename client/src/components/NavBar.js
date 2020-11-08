106eb984a05dbe01fb571670dff46c53ae1190f6
ahmtysn <ahmtysn14@gmail.com>
2020-11-08 14:45:14 +0100

    navbar-refresh

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { OpenContext } from '../components/OpenContext';
+import { BsHouseDoor } from 'react-icons/bs';
+import { IoMdRefresh } from 'react-icons/io';
+import { FaRegCommentDots } from 'react-icons/fa';
 
 function NavBar() {
+  const { open, setOpen } = useContext(OpenContext);
+
+  const resetStory = () => {
+    setOpen({
+      isOpenC1: false,
+      isOpenC2: false,
+      isOpenC3: false,
+      isOpenC4: false,
+    });
+  };
   return (
     <div className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/chapter1'>Part-I</Link>
-      <Link to='/chapter2'>Part-II</Link>
-      <Link to='/chapter3'>Part-III</Link>
-      <Link to='/chapter4'>Part-IV</Link>
-      <Link to='/form'>Form</Link>
+      <Link to='/'>
+        <BsHouseDoor />
+      </Link>
+      <Link to='/chapter1'>I</Link>
+      <Link to='/chapter2'>II</Link>
+      <Link to='/chapter3'>III</Link>
+      <Link to='/chapter4'>IV</Link>
+      <Link to='/' onClick={resetStory}>
+        <IoMdRefresh />
+      </Link>
+
+      <Link to='/form'>
+        <FaRegCommentDots />
+      </Link>
     </div>
   );
 }
