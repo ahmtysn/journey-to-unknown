d19a2bdf08917ba663bb61705db405ea5b84f6a8
ahmtysn <ahmtysn14@gmail.com>
2020-11-09 22:18:50 +0100

    style comments page

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -4,21 +4,19 @@ import { RiCloseCircleLine } from 'react-icons/ri';
 
 const Todo = ({ comments, removeComment }) => {
   return (
-    <div className='comments'>
+    <div className='comment-container'>
       {comments.map(comment => (
-        <div key={comment.id}>
-          <div className='comment'>
+        <div className='comment' key={comment.id}>
+          <div className='delete-text'>
             <div className='delete-icon'>
               <RiCloseCircleLine
                 onClick={() => removeComment(comment.id)}
                 className='delete-icon'
               />
             </div>
-            <div className='comment-text'>
-              <div>{comment.text}</div>
-              <div className='comment-time'>{comment.time}</div>
-            </div>
+            <div>{comment.text}</div>
           </div>
+          <div className='comment-time'>{comment.time}</div>
         </div>
       ))}
     </div>
