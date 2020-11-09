cecba6e0ccdbcf5103a3ac26c163ae9f323e922e
ahmtysn <ahmtysn14@gmail.com>
2020-11-09 22:58:15 +0100

    style images

--- client/src/components/comments/CommentHeader.js
+++ client/src/components/comments/CommentHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function CommentHeader() {
   return (
     <div className='comment-header'>
-      <h1>Welcome to Comments Section</h1>
+      <h1>Welcome to Comments</h1>
       <h4>Feel free yourself and share your opinion about story</h4>
     </div>
   );
